83d491e9b70143fc0b9a19d6ca0f049035cae34d
Juan Lafuente <jlafuente@telconet.ec>
2020-04-23 01:03:51 -0500

    Carga inicial del proyecto

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Adminpro
+# Lear Angular JNK
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.3.20.
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Rutas
+import { APP_ROUTES } from './app.routes';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { PagesComponent } from './pages/pages.component';
+
+import { HeaderComponent } from './shared/header/header.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './login/register.component';
+
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { Graficas1Component } from './pages/graficas1/graficas1.component';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		LoginComponent,
+		NopagefoundComponent,
+		DashboardComponent,
+		ProgressComponent,
+		Graficas1Component,
+		HeaderComponent,
+		SidebarComponent,
+		BreadcrumbsComponent,
+		PagesComponent,
+		RegisterComponent,
+	],
+	imports: [ BrowserModule, AppRoutingModule ],
+	providers: [],
+	bootstrap: [ AppComponent ],
 })
-export class AppModule { }
+export class AppModule {}
